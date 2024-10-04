8b8745e1bb9701f595031445ae80f5a54bed83d6
soniyaCNair <you@example.com>
2024-10-04 18:32:20 +0530

    task

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Script for additional interactions or smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
